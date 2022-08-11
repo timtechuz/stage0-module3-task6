f8b0140473cce73ad182d9ab5fda13359530d592
timtechuz <timur.galiaskarov@timtech.uz>
2022-08-11 17:28:14 +0500

    stage0-module3-task6 first attempt

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+    	long first = 1000;
+    	int second = 1000;
+    	
+    	first = second;
+    	    	
+    	System.out.println(first);
+    	System.out.println(second);
+    	
+    	
     }
 }
